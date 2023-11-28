f72a39a60467053024c039af71134932d0e767c5
gnxrlyqf <gnarlydestructor@outlook.com>
2023-11-28 16:56:09 +0100

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,5 +17,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	new->parent = parent;
 	new->n = value;
+	new->left = NULL, new->right = NULL;
 	return (new);
 }
