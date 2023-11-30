9607996c3f70dfb5e9eda760fc9f6692c41a22c8
gnxrlyqf <gnarlydestructor@gmail.com>
2023-11-30 11:47:56 -0800

    12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts all the leaves in a binary tree
+ * @tree: root node of the binary tree
+ *
+ * Return: number of leaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaf = 0;
+
+	if (tree)
+	{
+		if (!tree->left && !tree->right)
+			leaves += 1;
+		leaf += binary_tree_leaves(tree->left);
+		leaf += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
+}
+
