0955d01c52f4bcfbc98c31bf877b599343d5faf1
gnxrlyqf <gnarlydestructor@gmail.com>
2023-11-30 12:57:27 -0800

    3

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts all the leaves in a binary tree
+ * binary_tree_nodes - counts all the nodes in a binary tree
  * @tree: root node of the binary tree
  *
- * Return: number of leaves
+ * Return: number of nodes
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		if (!tree->left || !tree->right)
+		if (tree->left || tree->right)
 			node += 1;
 		node += binary_tree_nodes(tree->left);
 		node += binary_tree_nodes(tree->right);
