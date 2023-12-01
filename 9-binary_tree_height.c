313f8977e0f8864cd73f810b8db161b8f65e4c70
gnxrlyqf <gnarlydestructor@gmail.com>
2023-11-30 17:26:42 -0800

    14

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,12 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		if (tree->left)
 			left = 1 + binary_tree_height(tree->left);
-		else
-			left = 0;
 		if (tree->right)
 			right = 1 + binary_tree_height(tree->right);
-		else
-			right = 0;
 		if (left > right)
 			return (left);
 		else
