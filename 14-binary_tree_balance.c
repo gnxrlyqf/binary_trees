466dcd5b6db429043c87fa6c63bc7177e39f0cf4
gnxrlyqf <gnarlydestructor@gmail.com>
2023-11-30 17:28:22 -0800

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,31 @@
 #include "binary_trees.h"
 
 /**
+ * height - measures binary tree height
+ * @tree: root node of the tree to measure
+ *
+ * Return: tree size
+*/
+
+size_t height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left = 0, right = 0;
+
+		if (tree->left)
+			left = 1 + height(tree->left);
+		if (tree->right)
+			right = 1 + height(tree->right);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	return (0);
+}
+
+/**
  * binary_tree_balance - measures how balanced the tree is
  * @tree: root node of the tree
  *
@@ -10,7 +35,6 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
-
+		return (height(tree->left) - height(tree->right));
 	return (0);
 }
